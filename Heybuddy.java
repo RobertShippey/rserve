f181911d378084c5782b5462c18e2b2d224a8289
Robert Shippey <robert@robertshippey.net>
2011-11-23 19:30:54 +0000

    Update Hey buddy to include switched Hello World

--- Heybuddy.java
+++ Heybuddy.java
@@ -1,8 +1,37 @@
+import java.io.*;
 public class Heybuddy
 {
- public static void main (String[] args)
-{
- System.out.println("Hey Buddy");
+    public static void main (String[] args)
+    {
+        Boolean old = null;
+        old = IsSwitchValued(args, "-o");
+        try
+        {
+            if(old)
+            {
+                System.out.println("Hello World!");
+            }
+            else
+            {
+                System.out.println("Hey Buddy.");
+            }
+        }
+        catch (Exception e)
+        {
+            System.err.println("Something's wrong.");
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public static boolean IsSwitchValued(String[] args, String strSwitch)
+	{
+		for(int i = 0; i < args.length; i++)
+		{
+			if(args[i].equals(strSwitch))
+				return true;
+		}
+		return false;
+	}
+    
 }
 
-}

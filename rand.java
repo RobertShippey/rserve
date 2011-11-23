34a2fb7729d88b5a156def940278106d5b25f391
Robert Shippey <robert@robertshippey.net>
2011-11-23 18:06:44 +0000

    Add args param option

--- rand.java
+++ rand.java
@@ -1,16 +1,25 @@
 import java.util.Random;
 public class rand 
 {
-
-public static void main (String args[])
-{
-   Random r = new Random();
-   for(int i=0;i<r.nextInt(10);i++)
-{
-   System.out.print("Your random number is:");
-   System.out.println(r.nextInt(10));
-
-}
-}
-
+   public static void main (String args[])
+   {
+      Random r = new Random();
+      try
+      {
+         for i=0;i<args[0];x++)
+         {
+            System.out.print("Your random number is:");
+            System.out.println(r.nextInt(10));
+         }
+      }
+      catch (ArrayIndexOutOfBoundsException ex)
+      {
+         for(int i=0;i<r.nextInt(10);i++)
+         {
+            System.out.print("Your random number is:");
+            System.out.println(r.nextInt(10));
+            
+         }
+      }      
+   }
 }
